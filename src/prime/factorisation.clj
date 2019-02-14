40d2ef8bc068a5d05e79049b4269f5750a1b622a
Smelly Cat <smelly@c.at>
2019-02-15 10:36:36 +1100

    Implement primes up to n

--- src/prime/factorisation.clj
+++ src/prime/factorisation.clj
@@ -1,4 +1,10 @@
-(ns prime.factorisation)
+(ns prime.factorisation
+  (:require [prime.primality :refer [prime?]]))
+
+(defn primes-up-to [n]
+  (->> (inc n)
+       (range 2)
+       (filter prime?)))
 
 (defn factors-of [n]
   [1])
--- test/prime/factorisation_test.clj
+++ test/prime/factorisation_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [prime.factorisation :refer :all]))
 
+(deftest all-primes-up-to-n
+  (testing "n is non-prime"
+           (is (= [2 3 5 7 11] (primes-up-to 12))))
+
+  (testing "n itself is included if prime"
+           (is (= [2 3 5 7] (primes-up-to 7)))))
+
 (deftest one
   (testing "factor of 1 is 1"
            (is (= [1] (factors-of 1)))))
