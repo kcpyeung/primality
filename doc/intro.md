7ad93a9dd2c9bbe792b85769570eed679159afc6
Smelly Cat <smelly@c.at>
2019-02-14 11:23:22 +1100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to primality
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/primality/core_test.clj
@@ -0,0 +1,7 @@
+(ns primality.core-test
+  (:require [clojure.test :refer :all]
+            [primality.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
